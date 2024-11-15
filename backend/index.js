3a6b91a80baa42052fac7d8c9da420ccd3edc7bb
Lucky Mbithi <luckymbithi002@gmail.com>
2024-11-15 12:18:56 +0300

    configure and build application for production

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from "express";
+import path from "path";
 import { connectDb } from "./db/connectDB.js";
 import authRoutes from './routes/auth.route.js';
 
@@ -8,16 +9,19 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 app.use(express.json({ limit: "5mb" }))
 app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: "Server is running"
+if (process.env.NODE_ENV = "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
     })
-})
+}
 
 app.listen(PORT, () => {
     console.log(`server is running on port: ${PORT}`);
